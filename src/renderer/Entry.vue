9a85844c69d407b4fb73eda32fd4738506401efe
kamir222 <katia.amir@gmail.com>
2019-08-14 16:10:46 -0400

    Persist action clicks

--- src/renderer/Entry.vue
+++ src/renderer/Entry.vue
@@ -5,28 +5,22 @@
         <th v-for="(header, index) in data.headers" :key="index">{{header}}</th>
       </tr>
       <tr v-for="(row, rowIndex) in data.rows" :key="rowIndex">
-        <td
-          v-for="(header, headerIndex) in data.headers"
-          :key="headerIndex"
-        >
-          <button 
+        <td v-for="(header, headerIndex) in data.headers" :key="headerIndex">
+          <button
             v-if="header === 'need'"
             @click="onClick('need', rowIndex)"
-          >
-            need
-          </button>
-          <button 
+            v-bind:class="{ 'active-button': data.rows[rowIndex]['need']}"
+          >need</button>
+          <button
             v-else-if="header === 'want'"
             @click="onClick('want', rowIndex)"
-          >
-            want
-          </button>
-          <button 
+            v-bind:class="{ 'active-button': data.rows[rowIndex]['want']}"
+          >want</button>
+          <button
             v-else-if="header === 'saving'"
             @click="onClick('saving', rowIndex)"
-          >
-            saving
-          </button>
+            v-bind:class="{ 'active-button': data.rows[rowIndex]['saving']}"
+          >saving</button>
           <div v-else-if="row[header]">{{row[header]}}</div>
           <div v-else>N/A</div>
         </td>
@@ -35,39 +29,45 @@
   </article>
 </template>
 
+<style>
+.active-button {
+  background-color: red;
+  color: white;
+}
+</style>
+
 <script>
 import Vue from "vue";
 import _ from "lodash";
 import { remote } from "electron";
 import { ipcRenderer } from "electron";
 
-export default Vue.component('entry', {
-  name: 'entry',
-  data: function () {
+export default Vue.component("entry", {
+  name: "entry",
+  data: function() {
     return {
-      data: [],
-    }
+      data: []
+    };
   },
   methods: {
-    onClick: function (type, rowIndex) {
-      console.log(type)
-      console.log(rowIndex)
-      // To-do: modify data to mark the selected row as a type. 
-      // I was going to use the map function but I'm going to 
-      // do some slicing instead. 
-    },
+    onClick: function(action, rowIndex) {
+      this.data.rows[rowIndex]["need"] = false;
+      this.data.rows[rowIndex]["want"] = false;
+      this.data.rows[rowIndex]["saving"] = false;
+      this.data.rows[rowIndex][action] = true;
+    }
   },
   created() {
-      remote.dialog.showOpenDialog(
-        { properties: ["openFile", "openDirectory", "multiSelections"] },
-        filePaths => {
-          if (filePaths === undefined) {
-            alert("You didn't select a folder");
-            return;
-          }
-          ipcRenderer.send("extract-csv-data", filePaths);
+    remote.dialog.showOpenDialog(
+      { properties: ["openFile", "openDirectory", "multiSelections"] },
+      filePaths => {
+        if (filePaths === undefined) {
+          alert("You didn't select a folder");
+          return;
         }
-      )
+        ipcRenderer.send("extract-csv-data", filePaths);
+      }
+    );
 
     ipcRenderer.on("extract-csv-data-reply", (event, arg) => {
       if (arg.error !== undefined && arg.error) {
@@ -78,7 +78,5 @@ export default Vue.component('entry', {
       }
     });
   }
-})
-
-
+});
 </script>
